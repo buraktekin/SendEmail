be7306e0feab637a3fcf4abb42839250044240ae
buraktekin <tknbrk@gmail.com>
2013-11-07 08:09:51 +0200

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -1,5 +1,5 @@
 __author__ = 'Burak Tekin'
-__contact__ = 'iletisim@buraktekin.net
+__contact__ = 'iletisim@buraktekin.net'
 # Department of Computer Science
 # Sending Email Using SMTP Authentication
 
